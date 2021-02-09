312bc5b5818cf96a6601e6da49e4ac61a0b0a113
Yvonne Yeh <yvonneyeh@me.com>
2021-02-09 23:04:35 +0000

    Create data set for all friends

--- 3/madlibs/madlibs.py
+++ 3/madlibs/madlibs.py
@@ -35,12 +35,58 @@ def greet_person():
 
     player = request.args.get("person")
 
+    # game_response = request.args.get("game_response")
+
     compliment = choice(AWESOMENESS)
 
     return render_template("compliment.html",
                            person=player,
                            compliment=compliment)
 
+@app.route('/game')
+def show_madlib_form():
+    """Get user's response to game form"""
+
+    game_response = request.args.get("response")
+
+    if game_response == "no":
+        return render_template("goodbye.html")
+
+    if game_response == "yes":
+        return render_template("game.html")
+
+
+@app.route('/madlib')
+def show_madlib():
+
+    color = request.args.get("color")
+    pet = request.args.get("pet")
+    person = request.args.get("person")
+    adjective = request.args.get("adjective")
+    month = request.args.get("month")
+    petname = request.args.get("petname")
+    prog_lang = request.args.get("prog_lang")
+    brand = request.args.get("brand")
+    past_tense_verb = request.args.get("past_tense_verb")
+    food = request.args.get("food")
+
+    return render_template("madlib.html",
+                            color=color,
+                            pet=pet,
+                            person=person,
+                            adjective=adjective1,
+                            adjective=adjective2,
+                            adjective=adjective3,
+                            adjective=adjective4,
+                            adjective=adjective5,
+                            month=month,
+                            petname=petname,
+                            prog_lang=prog_lang,
+                            brand=brand,
+                            past_tense_verb=past_tense_verb,
+                            food=food)
+
+     
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
---
+++ 5/react-trading-cards/static/js/tradingCard.jsx
@@ -0,0 +1,122 @@
+"use strict";
+const tradingCardData = [
+  {
+    name: 'Balloonicorn',
+    skill: 'video games',
+    imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
+  }
+];
+
+function TradingCard(props) {
+  return (
+    <div className="card">
+      <h2>Name: {props.name}</h2>
+      <img src={props.imgUrl} />
+      <h2>Skill: {props.skill}</h2>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Balloonicorn"
+      skill="video games"
+      imgUrl="/static/img/balloonicorn.jpg"
+    />
+  ),
+  document.querySelector('#balloonicorn')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Float"
+      skill="baking pretzels"
+      imgUrl="/static/img/float.jpg"
+    />
+  ),
+  document.querySelector('#float')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Llambda"
+      skill="knitting scarves"
+      imgUrl="/static/img/llambda.jpg"
+    />
+  ),
+  document.querySelector('#llambda')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="Shortstack Overflow"
+      skill="telling jokes"
+      imgUrl="/static/img/shortstack-overflow.jpg"
+    />
+  ),
+  document.querySelector('#shortstack')
+);
+
+ReactDOM.render(
+  (
+    <TradingCard
+      name="SeedPy"
+      skill="juggling"
+      imgUrl="/static/img/seedpy.jpg"
+    />
+  ),
+  document.querySelector('#seedpy')
+);
+
+// for (const data of tradingCardData) {
+//   console.log(data.name);
+//   console.log(data.skill);
+// }
+
